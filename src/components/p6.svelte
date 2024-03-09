6a4f059f7c9992906bb1cf42a7fe9bd2812f5317
Kurumi Kaneko <kurumi@Kurumis-MacBook.lan>
2024-03-09 00:16:19 -0800

    slight change to page6

--- src/components/p6.svelte
+++ src/components/p6.svelte
@@ -7,14 +7,14 @@
 <h1>Our Suggestions</h1>
 
 <div id="top-right-text" 
-        style="position: absolute; top: 120px; left: 400px; right: 60px; font-size: 14px; border: 1px solid #ccc; padding: 10px;">
+        style="position: absolute; top: 120px; left: 800px; right: 60px; font-size: 14px; border: 1px solid #ccc; padding: 10px;">
             <strong>Step 5:</strong> Empower Through Insight - Can we absorb all of this from current warnings alone? Not entirely!
             Hence, we offer these suggestions to empower UCSD students, fostering
             awareness and self-protection
 </div>
 
 <div id="email">
-        <img src='page6_email.png' alt="Suggestion Email" style="width: 100%; height: auto;"/>
+        <img src='page6_email1.png' alt="Suggestion Email" style="width: 100%; height: auto;"/>
 </div>
 
 
