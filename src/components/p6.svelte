d25aa589d322e71cf8fde6ac6326f270ef9ee614
Kurumi Kaneko <kurumi@Kurumis-MacBook.lan>
2024-03-08 22:56:48 -0800

    suggestion email on page 6

--- src/components/p6.svelte
+++ src/components/p6.svelte
@@ -6,14 +6,16 @@
 
 <h1>Our Suggestions</h1>
 
-<p>split the page into two. one for adding maps and one for related crimes in the area</p>
-<p>have not started but I have an idea in my head that'll prob implement on fri</p>
+<div id="email">
+        <!-- Use Mapbox Static Images API to generate a static image -->
+        <img src='page6_email.png' alt="Suggestion Email" style="width: 100%; height: auto;"/>
+</div>
 
-<div id="top-right-text" style="position: absolute; top: 120px; 
-        right: 60px; font-size: 14px; border: 1px solid #ccc; 
-        padding: 10px;">
+
+<div id="top-right-text" 
+        style="position: absolute; top: 120px; right: 60px; font-size: 14px; border: 1px solid #ccc; padding: 10px;">
             Top Right Text</div>
-            
+
 </main>
 
 <style>
