21dd94cb6ccfd80cbccb2e79de0f37dcd96fbcd8
Kurumi Kaneko <kurumi@Kurumis-MacBook.lan>
2024-03-09 19:11:23 -0800

    page6 textbox

--- src/components/p6.svelte
+++ src/components/p6.svelte
@@ -8,7 +8,10 @@
 
 <div id="top-right-text" 
         style="position: absolute; top: 120px; left: 800px; right: 60px; font-size: 14px; border: 1px solid #ccc; padding: 10px;">
-            This is what we think UCPD could do to enhance Triton Alerts. 
+            These are our proposed changes to the Triton Alerts that we are currently receiving. Firstly, we think it is crucial to add a 
+            map of the location so that it is more clear to all academics, staff, and students if they are in a dangerous area that they 
+            should vacate. We also think we should include a list of crimes that happened in the area recently so that they can make an 
+            informed decision.
 </div>
 
 <div id="email">
