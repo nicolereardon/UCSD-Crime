55eaa097d3adec863dde0448b2ed19149a58b3ec
Kurumi Kaneko <kurumi@Kurumis-MacBook.lan>
2024-03-14 11:31:16 -0700

    change page7 style

--- src/components/p7.svelte
+++ src/components/p7.svelte
@@ -5,42 +5,34 @@
 </script>
 
 <main>
-    <h1>Map View</h1>
     
     <div class="map-container">
-        <svg class="map-svg" viewBox="0 0 400 400">
+        <svg class="map-svg" viewBox="0 0 800 400">
             <!-- Embed the map image here -->
             <image href={`https://api.mapbox.com/styles/v1/mapbox/light-v11/static/${input_center[0]},${input_center[1]},13,0,0/400x400?access_token=pk.eyJ1Ijoia3NrYW5la28iLCJhIjoiY2xzZm4ycm01MGtjYTJqcHFsMXl1enNjcCJ9.20jJnxwrWnDVKl-EZOGVew`} width="100%" height="100%" />
+            <foreignObject x="39%" y="38%" width="100%" height="100%">
+                <div class="conclusion">
+                    <h1>Bye Bye Crime</h1>
+                </div>
+            </foreignObject>
         </svg>
     </div>
 </main>
 
-<div id="top-right-text">
-    Let's get this to a better spot and figure out what to say
-</div>
-
 <style>
-    #top-right-text {
-        position: absolute;
-        top: 140px;
-        left: calc(50%);
-        transform: translateX(50%);
-        width: 300px; /* Adjust as needed */
-        border: 3px solid #ADD8E6;
-        padding: 10px;
-        background-color: white;
-        font-family: 'Lato', sans-serif;
-        line-height: 1.3;
-        font-size: 18px;
-    }
-
     .map-container {
-        width: 100%; /* Adjust the map size */
-        margin: auto; /* Center horizontally */
+        width: 100%;
+        height: 100%;
     }
 
     .map-svg {
         width: 100%;
         border: 1px solid #ddd;
+    }
+
+    .conclusion {
+        position: absolute;
+        text-align: center;
+        z-index: 1;
     }
 </style>
