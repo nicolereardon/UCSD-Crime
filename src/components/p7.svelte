afc0af20b3270865375e4b2e2ef47b33fe19fbb3
unknown <jhauk@ucsd.edu>
2024-03-14 10:51:23 -0700

    ugly verson

--- src/components/p7.svelte
+++ src/components/p7.svelte
@@ -4,10 +4,18 @@
     let input_center = [-117.23685272044901, 32.87940699468673];
 </script>
 
-<div>
-    <img src={`https://api.mapbox.com/styles/v1/mapbox/light-v1/static/${input_center[0]},${input_center[1]},16,0,0/500x500?access_token=pk.eyJ1Ijoia3NrYW5la28iLCJhIjoiY2xzZm4ycm01MGtjYTJqcHFsMXl1enNjcCJ9.20jJnxwrWnDVKl-EZOGVew`} width="100%" height="100%" />
-</div>
+<main>
+    <h1>Map View</h1>
+    
+    <svg class="map-svg" viewBox="0 0 500 500">
+        <!-- Embed the map image here -->
+        <image href={`https://api.mapbox.com/styles/v1/mapbox/navigation-day-v1/static/${input_center[0]},${input_center[1]},13,0,0/500x500?access_token=pk.eyJ1Ijoia3NrYW5la28iLCJhIjoiY2xzZm4ycm01MGtjYTJqcHFsMXl1enNjcCJ9.20jJnxwrWnDVKl-EZOGVew`} width="100%" height="100%" />
+    </svg>
+</main>
 
 <style>
-
+    .map-svg {
+        width: 100%;
+        border: 1px solid #ddd;
+    }
 </style>
