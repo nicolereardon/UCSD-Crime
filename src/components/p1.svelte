e8685ade0f1509842dbd5cc40a26091a15e411d5
Kurumi Kaneko <kurumi@Kurumis-MacBook.lan>
2024-03-14 09:46:34 -0700

    change oolor of hook on page 1

--- src/components/p1.svelte
+++ src/components/p1.svelte
@@ -2,9 +2,13 @@
 
 </script>
 
-<div>
-    <h1 class="intro_text">In the National Council for Home and Safety and Security’s 2022 Safest College Campuses in the United States report, the University of California, San Diego was ranked 476th out of 490 colleges. However, San Diego was ranked the 95th safest city out of 3,381 in California in their 2021 Safest Cities in California report. Of the 10 UCs, UCSD places 8th in safety but is in the 2nd safest city.</h1>
-    <h2 class="intro_text">What does UCSD do about crime on campus?</h2>
+<div class= "intro_page">
+    <h1 class="intro_text">In the National Council for Home and Safety and Security’s 2022 Safest College Campuses in the United States report, 
+		the University of California, San Diego was ranked 476th out of 490 colleges. 
+		However, San Diego was ranked the 95th safest city out of 3,381 in California in their 2021 Safest Cities in California report. 
+		<span class="hook"> Of the 10 UCs, UCSD places 8th in safety but is in the 2nd safest city </span>.</h1>
+	<img scr='safety_rankings.png' alt="Safety Rankings">
+	<h2 class="intro_text">What does UCSD do about crime on campus?</h2>
     <!-- <h3 class="intro_text">From Lamine Secka, Chief of Police of the UC San Deigo Police Department (UCPD), they are committed to providing public safety through “community partnerships, innovative collaborations, and modern policing practices” which includes proper reporting of crimes.</h3> -->
 	<h4 class="intro_text">As required by the Clery Act, UCSD maintains the “TRITON ALERT Emergency Notification” system which is used to immediately inform the UC San Diego community upon confirmation of an emergency or dangerous situation that involves an immediate threat to the health or safety of students or employees. </h4>
 	<h5 class="intro_text">Once initiated, all UC San Diego students, faculty, and staff are notified through email and, optionally, text message of a Timely Warning or Community Alert Bulletin with a brief description of the emergency and instructions about what to do.</h5>
@@ -12,18 +16,21 @@
 </div>
 
 <style>
-	.intro_page {
-	  display: flex;
-	  flex-direction: column;
-	  justify-content: center;
-	  align-items: center;
-	  height: 100vh; 
-	}
-  
+
+
 	.intro_text {
 	  font-size: 20px;
 	  text-align: center;
 	  font-family: 'Roboto', sans-serif;
 	  margin-bottom: 20px;
+	}
+
+	.hook {
+		color: blue;
+	}
+
+	img {
+		max-width: 50%;
+		margin-right: 20px;
 	}
 </style>
