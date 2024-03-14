7363dfd1276a0f5f2281199ba082488f1273cf2f
unknown <jhauk@ucsd.edu>
2024-03-14 11:05:52 -0700

    hehe

--- src/components/p7.svelte
+++ src/components/p7.svelte
@@ -7,13 +7,38 @@
 <main>
     <h1>Map View</h1>
     
-    <svg class="map-svg" viewBox="0 0 500 500">
-        <!-- Embed the map image here -->
-        <image href={`https://api.mapbox.com/styles/v1/mapbox/light-v11/static/${input_center[0]},${input_center[1]},13,0,0/400x400?access_token=pk.eyJ1Ijoia3NrYW5la28iLCJhIjoiY2xzZm4ycm01MGtjYTJqcHFsMXl1enNjcCJ9.20jJnxwrWnDVKl-EZOGVew`} width="100%" height="100%" />
-    </svg>
+    <div class="map-container">
+        <svg class="map-svg" viewBox="0 0 400 400">
+            <!-- Embed the map image here -->
+            <image href={`https://api.mapbox.com/styles/v1/mapbox/light-v11/static/${input_center[0]},${input_center[1]},13,0,0/400x400?access_token=pk.eyJ1Ijoia3NrYW5la28iLCJhIjoiY2xzZm4ycm01MGtjYTJqcHFsMXl1enNjcCJ9.20jJnxwrWnDVKl-EZOGVew`} width="100%" height="100%" />
+        </svg>
+    </div>
 </main>
 
+<div id="top-right-text">
+    Let's get this to a better spot and figure out what to say
+</div>
+
 <style>
+    #top-right-text {
+        position: absolute;
+        top: 140px;
+        left: calc(50%);
+        transform: translateX(50%);
+        width: 300px; /* Adjust as needed */
+        border: 3px solid #ADD8E6;
+        padding: 10px;
+        background-color: white;
+        font-family: 'Lato', sans-serif;
+        line-height: 1.3;
+        font-size: 18px;
+    }
+
+    .map-container {
+        width: 100%; /* Adjust the map size */
+        margin: auto; /* Center horizontally */
+    }
+
     .map-svg {
         width: 100%;
         border: 1px solid #ddd;
